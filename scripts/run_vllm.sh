74420aabd6cefc808768b13ac7bb7e795be7f45d
Syd <sydpeng1@gmail.com>
2025-12-17 21:03:06 -0500

    updates

---
+++ scripts/run_vllm.sh
@@ -0,0 +1,13 @@
+nsys profile \
+  -t cuda \
+  -s none \
+  --cpuctxsw=none \
+  --trace-fork-before-exec=true \
+  --force-overwrite=true \
+  --stats=true \
+  -o moe_dp1tp4 \
+  vllm serve /pscratch/sd/c/cp724/DeepSeek-V2-Lite \
+    --tensor-parallel-size 1 \
+    --data-parallel-size 4 \
+    --enable-expert-parallel \
+    --port 8000
