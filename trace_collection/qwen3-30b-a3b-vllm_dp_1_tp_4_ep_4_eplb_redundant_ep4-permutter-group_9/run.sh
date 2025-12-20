f1eda5733ee25fd0f19e24e73d72328c4a64009b
InfiniteQvQ <158520207+InfiniteQvQ@users.noreply.github.com>
2025-12-20 00:44:16 -0500

    add eplb

---
+++ trace_collection/qwen3-30b-a3b-vllm_dp_1_tp_4_ep_4_eplb_redundant_ep4-permutter-group_9/run.sh
@@ -0,0 +1,16 @@
+
+nsys profile \
+  -t cuda \
+  -s none \
+  --cpuctxsw=none \
+  --force-overwrite=true \
+  --stats=true \
+  -o default_eplb4 \
+  vllm serve /pscratch/sd/j/jy2222/Qwen3-30B-A3B \
+    --data-parallel-size 4 \
+    --tensor-parallel-size 1 \
+    --enable-expert-parallel \
+    --served-model-name Qwen \
+    --gpu-memory-utilization 0.85 \
+    --enable-eplb \ 
+    --eplb-config '{"window_size":1000,"step_interval":3000,"num_redundant_experts":4,"log_balancedness":false}'
