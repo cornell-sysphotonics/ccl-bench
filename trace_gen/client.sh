f35471909f833939936686b880f3ecb363cb11e2
RadioMediocre <qiwen108@gmail.com>
2025-12-20 20:45:10 -0800

    delete unneeded files

--- trace_gen/client.sh
+++
@@ -1,7 +0,0 @@
-python3 benchmark.py --backend vllm \
-    --model Qwen/Qwen3-32B \
-    --request-rate 1 \
-    --num-prompts 10 \
-    --dataset-name dummy \
-    --long-prompts 0 \
-    --long-prompt-len 32000
