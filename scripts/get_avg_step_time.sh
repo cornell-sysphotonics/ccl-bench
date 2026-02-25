6eec5acecc93074b843deba2571ad59e22815d33
EricDinging <dd687@cornell.edu>
2026-02-24 23:32:34 -0500

    Add workloads

--- scripts/get_avg_step_time.sh
+++ scripts/get_avg_step_time.sh
@@ -4,4 +4,4 @@ python ./tools/main.py --trace "/data/ccl-bench_trace_collection/Llama-3.1-8B-to
 
 python ./tools/main.py --trace "/data/ccl-bench_trace_collection/Llama-3.1-8B-torchxla-vllm-tp2-tpu-group-4" --metric "avg_step_time"
 
-python ./tools/main.py --trace "/data/ccl-bench_trace_collection/deepseek-v3-16b-torchtitan-perlmutter" --metric "avg_step_time"
+python ./tools/main.py --trace "/data/ccl-bench_trace_collection/deepseek-v3-16b-torchtitan-ep8-dp8-perlmutter" --metric "avg_step_time"
